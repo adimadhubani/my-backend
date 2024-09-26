22e2573bd9b17b9ce13415a942c8437c70b5bd69
Aditya Kumar <adityakumar@Adityas-MacBook-Air-2.local>
2024-09-26 13:12:50 +0530

    utils code

--- src/index.js
+++ src/index.js
@@ -10,7 +10,14 @@ dotenv.config({
     path:'./env'
 })
 
-CONNECTDB();
+CONNECTDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`server is running at port: ${process.env.PORT}`);
+    })
+}).catch((err)=>{
+    console.log("MONGODB CONNECTON FAILED !!!", err);
+})
 
 /*(async()=>{
     try{
